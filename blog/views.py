5319750fe35066923286f9780b40148d6e68fd0a
amirreza13812003 <hosseiniseyyedamirreza2003@gmail.com>
2024-08-13 12:22:26 +0330

    add comments to blog

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-from .models import Post
-from .forms import EmailPostForm
+from django.views.decorators.http import require_POST
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
 
 
 # def post_list(request):
@@ -35,8 +36,12 @@ def post_detail(request, post, year, month, day):
         publish__month=month,
         publish__day=day
     )
-    
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)    
+    # comments = Comment.objects.filter(post=post).filter(active=True)
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -68,4 +73,19 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
 
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})            
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment': comment})
+    
